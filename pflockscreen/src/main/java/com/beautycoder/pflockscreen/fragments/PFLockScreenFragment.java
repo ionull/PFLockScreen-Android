590739b64791f241f8d7f26c3c04f14563847720
Tsung Wu <tsung.bz@gmail.com>
2024-04-11 22:14:40 +0800

    fix: should not delay on start

--- pflockscreen/src/main/java/com/beautycoder/pflockscreen/fragments/PFLockScreenFragment.java
+++ pflockscreen/src/main/java/com/beautycoder/pflockscreen/fragments/PFLockScreenFragment.java
@@ -14,7 +14,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Vibrator;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -129,9 +128,7 @@ public class PFLockScreenFragment extends Fragment {
         if (!mIsCreateMode && mUseFingerPrint && mConfiguration.isAutoShowFingerprint() &&
                 isFingerprintApiAvailable(getActivity()) && isFingerprintsExists(getActivity())) {
             //Log.e("AppActivityLog", "onPFLockStart show biometric " + getActivity() + this);
-            new Handler().postDelayed(() -> {
-                mOnFingerprintClickListener.onClick(mFingerprintButton);
-            }, 200);
+            mOnFingerprintClickListener.onClick(mFingerprintButton);
         }
     }
 
